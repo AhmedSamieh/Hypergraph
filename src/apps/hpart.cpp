14274d367c56a4bb66bac92a611fc933126679b3
Ahmed Samieh <ahmed.samieh@gmail.com>
2015-12-12 03:19:23 +0200

    update hpart to get the end of partitions

--- src/apps/hpart.cpp
+++ src/apps/hpart.cpp
@@ -6,8 +6,8 @@
  *          contains test app for hypergraph_load, graph_partition
  ******************************************************************************/
 #include "hypergraph_file.h"
-//#include "graph_partition.h"
 #include <set>
+#include <cstdlib>
 
 using namespace std;
 
@@ -16,17 +16,19 @@ int main(int argc, char **argv)
     int  nedges = 0, nvtxs = 0;
     int *eptr = NULL, *eind = NULL;
 
-    if (argc == 1)
+    /*if (argc == 2)
     {
         hypergraph_load(stdin, &nedges, &nvtxs, &eptr, &eind);
     }
-    else if (argc == 2)
+    else*/if (argc == 3)
     {
         hypergraph_load(argv[1], &nedges, &nvtxs, &eptr, &eind);
     }
     set<int> internal_nodes;
     set<int> external_nodes;
     //int start = 0, end = 0, ports = nodes;
+    double old_ratio = 0.0, new_ratio = 0.0;
+    int number_of_partitions = 0;
     for (int i = 0; i < nedges; i++)
     {
         //printf("insert %d into internal and remove it from external\n", i);
@@ -45,12 +47,38 @@ int main(int argc, char **argv)
                (int)internal_nodes.size(),
                (int)external_nodes.size(),
                (int)(external_nodes.size() > 0 ? (internal_nodes.size()/external_nodes.size()) : internal_nodes.size()));*/
-        printf("%d %d %d\n",
+        new_ratio = external_nodes.size() > 0 ? (((float)internal_nodes.size())/external_nodes.size()) : (float)internal_nodes.size();
+        if (old_ratio >= atof(argv[2]) && new_ratio < old_ratio)
+        {
+            printf("partition %d : internal nodes = %d, external_nodes = %d\n", ++number_of_partitions,
+                   (int)internal_nodes.size(),
+                   (int)external_nodes.size());
+            internal_nodes.clear();
+            internal_nodes.insert(i);
+            external_nodes.clear();
+            for (int j = eptr[i]; j < eptr[i + 1]; j++)
+            {
+                if (internal_nodes.find(eind[j]) == internal_nodes.end())
+                {
+                    //printf("insert %d into external\n", eind[j]);
+                    external_nodes.insert(eind[j]);
+                }
+            }
+            old_ratio = ((float)internal_nodes.size())/external_nodes.size();
+        }
+        else
+        {
+            old_ratio = new_ratio;
+        }
+        /*printf("%d %d %g\n",
                (int)internal_nodes.size(),
                (int)external_nodes.size(),
-               (int)(external_nodes.size() > 0 ? (internal_nodes.size()/external_nodes.size()) : internal_nodes.size()));
+               (external_nodes.size() > 0 ? (((float)internal_nodes.size())/external_nodes.size()) : (float)internal_nodes.size()));*/
     }
-
+    //printf("end of partition %d\n", number_of_partitions++);
+    printf("partition %d : internal nodes = %d, external_nodes = %d\n", ++number_of_partitions,
+           (int)internal_nodes.size(),
+           (int)external_nodes.size());
     delete[] eptr;
     delete[] eind;
     return 0;
--- src/apps/masters.sh
+++ src/apps/masters.sh
@@ -5,14 +5,14 @@ FILE_NAME=Linear
 
 ./horder_cm ${FILE_NAME}_sparse_matrix_org.txt > ${FILE_NAME}_sparse_matrix_cm.txt
 ./hpairs ${FILE_NAME}_sparse_matrix_cm.txt > ${FILE_NAME}_sparse_matrix_cm_pairs.txt
-./hpart ${FILE_NAME}_sparse_matrix_cm.txt > ${FILE_NAME}_sparse_matrix_cm_parts.txt
+./hpart ${FILE_NAME}_sparse_matrix_cm.txt 5.0 > ${FILE_NAME}_sparse_matrix_cm_parts.txt
 
 ./horder_rcm ${FILE_NAME}_sparse_matrix_org.txt > ${FILE_NAME}_sparse_matrix_rcm.txt
 ./hpairs ${FILE_NAME}_sparse_matrix_rcm.txt > ${FILE_NAME}_sparse_matrix_rcm_pairs.txt
-./hpart ${FILE_NAME}_sparse_matrix_rcm.txt > ${FILE_NAME}_sparse_matrix_rcm_parts.txt
+./hpart ${FILE_NAME}_sparse_matrix_rcm.txt 5.0 > ${FILE_NAME}_sparse_matrix_rcm_parts.txt
 
 ./hedges_metis ${FILE_NAME}_nodes_devices.txt > ${FILE_NAME}_sparse_matrix_org_for_metis.txt
 ./ndmetis ${FILE_NAME}_sparse_matrix_org_for_metis.txt
 ./horder_metis ${FILE_NAME}_sparse_matrix_org.txt ${FILE_NAME}_sparse_matrix_org_for_metis.txt.iperm > ${FILE_NAME}_sparse_matrix_mts.txt
 ./hpairs ${FILE_NAME}_sparse_matrix_mts.txt > ${FILE_NAME}_sparse_matrix_mts_pairs.txt
-./hpart ${FILE_NAME}_sparse_matrix_mts.txt > ${FILE_NAME}_sparse_matrix_mts_parts.txt
+./hpart ${FILE_NAME}_sparse_matrix_mts.txt 5.0 > ${FILE_NAME}_sparse_matrix_mts_parts.txt
